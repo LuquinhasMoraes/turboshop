0968eb44e322c07ba1d630338093100ce84362c3
LuquinhasMoraes <lucas_moraes2011@outlook.com>
2022-03-26 16:37:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ apps/docs/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Button, Header } from "ui";
+
+export default function Docs() {
+  return (
+    <div>
+      <Header />
+      <h1>Docs</h1>
+      <Button />
+    </div>
+  );
+}
---
+++ packages/ui/index.tsx
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from './components/Button'
+import { Header } from './components/Header'
+import { ProductCard } from './components/ProductCard'
+
+export {
+    Button,
+    Header,
+    ProductCard    
+}
